4d056311e67b42097f2821821f7629c6894e05a1
Sergey Abakumov <Sabakumov@binck.nl>
2020-12-19 14:14:46 +0100

    'created'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  AR_EXPORTS,
+  HtmlExportService,
+  PdfExportService,
+  ViewerComponent,
+  XlsxExportService,
+} from '@grapecity/activereports-angular';
+import { Core } from '@grapecity/activereports';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    '<div id="viewer-host"><gc-activereports-viewer (init)="onViewerInit()"> </gc-activereports-viewer></div> ',
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: AR_EXPORTS,
+      useClass: PdfExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: HtmlExportService,
+      multi: true,
+    },
+    {
+      provide: AR_EXPORTS,
+      useClass: XlsxExportService,
+      multi: true,
+    },
+  ],
 })
 export class AppComponent {
-  title = 'an24';
+  @ViewChild(ViewerComponent, { static: false }) reportViewer: ViewerComponent;
+  onViewerInit() {
+    Core.FontStore.registerFonts('assets/fontsConfig.json').then(() => {
+      this.reportViewer.open('assets/Frontstore.rdlx-json');
+    });
+  }
 }
